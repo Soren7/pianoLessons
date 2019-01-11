b388737229d41cf4063b31a525518ce84aee5900
maxwell <maxsolewis@gmail.com>
2019-01-11 13:11:57 -0700

    filped the images of in the dropdown

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 
 let alfredBooks = document.getElementById('alfred-books');
 let bookButton = document.getElementById('button');
+let arrow = document.getElementsByTagName('i');
 
 function dropDown () {
     if (alfredBooks.className === 'hide') {
         alfredBooks.className = 'example';
+        arrow.class.id = 'rotate';
     } else {
         alfredBooks.className = 'hide';
+        arrow.id = 'null';
     }
 }
 
